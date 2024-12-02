6c5faa805dcdf56fa04112185c3c05ea76137f8c
Mark Sommers <mark_sommers@apple.com>
2024-12-01 20:13:58 -0500

    Finished Day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,71 @@
+use std::{collections::HashMap, fs};
+
+fn parse_input(input: &str) -> (Vec<usize>, Vec<usize>) {
+    return input
+        .lines()
+        .map(|line| {
+            let (left, right) = line.split_once("   ").expect("invalid line");
+            (
+                left.parse::<usize>().expect("invalid usize"),
+                right.parse::<usize>().expect("invalid usize"),
+            )
+        })
+        .unzip();
+}
+
+fn part1(input: &str) -> usize {
+    let (mut left, mut right) = parse_input(input);
+
+    left.sort();
+    right.sort();
+
+    return left
+        .iter()
+        .zip(right.iter())
+        .map(|(left, right)| left.abs_diff(*right))
+        .sum();
+}
+
+fn part2(input: &str) -> usize {
+    let (left, right) = parse_input(input);
+
+    let right_count = right.iter().fold(HashMap::new(), |mut map, val| {
+        map.entry(*val).and_modify(|n| *n += 1).or_insert(1usize);
+        map
+    });
+
+    return left
+        .iter()
+        .map(|num| num * right_count.get(num).unwrap_or(&0))
+        .sum();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").unwrap();
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{part1, part2};
+
+    const INPUT: &str = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+
+    #[test]
+    fn test_part1() {
+        let res = part1(INPUT);
+        assert_eq!(res, 11);
+    }
+
+    #[test]
+    fn test_part2() {
+        let res = part2(INPUT);
+        assert_eq!(res, 31);
+    }
 }
