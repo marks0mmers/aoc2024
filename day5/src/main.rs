b6fad6f4cd3cc90d5e307ed281b4d6515634990f
Mark Sommers <mark_sommers@apple.com>
2024-12-05 10:55:34 -0500

    Optimized Day 5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -9,13 +9,17 @@ impl Update {
         Self { pages }
     }
 
+    fn middle_page(&self) -> usize {
+        return self.pages[self.pages.len() / 2];
+    }
+
     fn is_in_order(&self, ordering: &HashMap<usize, Vec<usize>>) -> bool {
         for (i, page) in self.pages.iter().enumerate() {
-            if let Some(ordering_for_page) = ordering.get(page) {
-                let remaining_pages = &self.pages[i + 1..];
-                if ordering_for_page
+            if let Some(page_order) = ordering.get(page) {
+                let remaining = &self.pages[i + 1..];
+                if page_order
                     .iter()
-                    .any(|order| self.pages.contains(order) && !remaining_pages.contains(order))
+                    .any(|order| self.pages.contains(order) && !remaining.contains(order))
                 {
                     return false;
                 }
@@ -26,102 +30,74 @@ impl Update {
     }
 
     fn fix_order(&self, ordering: &HashMap<usize, Vec<usize>>) -> Update {
-        let mut new_pages = self.pages.clone();
-        new_pages.sort_by(|a, b| {
-            if let Some(ordering_for_page_a) = ordering.get(a) {
-                if ordering_for_page_a.contains(b) {
-                    return Ordering::Less;
-                }
-            }
-            if let Some(ordering_for_page_b) = ordering.get(b) {
-                if ordering_for_page_b.contains(a) {
-                    return Ordering::Greater;
-                }
-            }
+        let mut pages = self.pages.clone();
 
+        pages.sort_by(|a, b| {
+            if ordering.get(a).map_or(false, |order| order.contains(b)) {
+                return Ordering::Less;
+            }
+            if ordering.get(b).map_or(false, |order| order.contains(a)) {
+                return Ordering::Greater;
+            }
             return Ordering::Equal;
         });
-        return Update { pages: new_pages };
-    }
 
-    fn middle_page(&self) -> usize {
-        return self.pages[self.pages.len() / 2];
+        return Update { pages };
     }
 }
 
-struct SafetyManual {
-    ordering: HashMap<usize, Vec<usize>>,
-    updates: Vec<Update>,
-}
+fn parse_input(input: &str) -> (HashMap<usize, Vec<usize>>, Vec<Update>) {
+    let (ordering_string, update_string) = input.split_once("\n\n").expect("invalid input");
 
-impl SafetyManual {
-    fn new(input: &str) -> Self {
-        let (ordering_string, update_string) = input.split_once("\n\n").expect("invalid input");
+    let mut ordering = HashMap::new();
 
-        let mut ordering = HashMap::new();
-
-        for (key, value) in ordering_string
-            .lines()
-            .filter_map(|line| line.split_once("|"))
-            .map(|(left, right)| {
-                (
-                    left.parse::<usize>().expect("invalid number"),
-                    right.parse::<usize>().expect("invalid number"),
-                )
-            })
-        {
-            ordering
-                .entry(key)
-                .and_modify(|values: &mut Vec<usize>| values.extend([value]))
-                .or_insert(vec![value]);
-        }
-
-        let updates = update_string
-            .lines()
-            .map(|line| {
-                Update::new(
-                    line.split(",")
-                        .filter_map(|num_str| num_str.parse::<usize>().ok())
-                        .collect(),
-                )
-            })
-            .collect::<Vec<_>>();
-
-        Self { ordering, updates }
+    for (key, value) in ordering_string
+        .lines()
+        .filter_map(|line| line.split_once("|"))
+        .map(|(left, right)| {
+            (
+                left.parse::<usize>().expect("invalid number"),
+                right.parse::<usize>().expect("invalid number"),
+            )
+        })
+    {
+        ordering
+            .entry(key)
+            .and_modify(|values: &mut Vec<usize>| values.extend([value]))
+            .or_insert(vec![value]);
     }
 
-    fn get_updates_in_order(&self) -> Vec<&Update> {
-        self.updates
-            .iter()
-            .filter(|update| update.is_in_order(&self.ordering))
-            .collect()
-    }
+    let updates = update_string
+        .lines()
+        .map(|line| {
+            Update::new(
+                line.split(",")
+                    .filter_map(|num_str| num_str.parse::<usize>().ok())
+                    .collect(),
+            )
+        })
+        .collect::<Vec<_>>();
 
-    fn get_updates_out_of_order(&self) -> Vec<&Update> {
-        self.updates
-            .iter()
-            .filter(|update| !update.is_in_order(&self.ordering))
-            .collect()
-    }
+    (ordering, updates)
 }
 
 fn part1(input: &str) -> usize {
-    let manual = SafetyManual::new(input);
+    let (ordering, updates) = parse_input(input);
 
-    return manual
-        .get_updates_in_order()
+    return updates
         .iter()
+        .filter(|update| update.is_in_order(&ordering))
         .map(|update| update.middle_page())
         .sum();
 }
 
 fn part2(input: &str) -> usize {
-    let manual = SafetyManual::new(input);
+    let (ordering, updates) = parse_input(input);
 
-    return manual
-        .get_updates_out_of_order()
+    return updates
         .iter()
-        .map(|update| update.fix_order(&manual.ordering).middle_page())
+        .filter(|update| !update.is_in_order(&ordering))
+        .map(|update| update.fix_order(&ordering).middle_page())
         .sum();
 }
 
