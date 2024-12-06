bc8f25c1bd44f9650241f1db6ab6fe2863422d86
Mark Sommers <mark_sommers@apple.com>
2024-12-06 13:21:49 -0500

    Finished Day 6 w/ Brute Force

---
+++ day6/src/main.rs
@@ -0,0 +1,223 @@
+use std::collections::HashMap;
+
+use utils::Vec2;
+
+#[derive(Debug, Clone, Copy)]
+enum Tile {
+    Blocked,
+    Empty,
+    Visited(Direction),
+}
+
+impl Tile {
+    fn new(c: char) -> Self {
+        match c {
+            '#' => Self::Blocked,
+            '.' => Self::Empty,
+            '^' => Self::Visited(Direction::North),
+            _ => panic!("invalid tile: {c}"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+impl Direction {
+    fn get_offset(&self) -> Vec2 {
+        match self {
+            Direction::North => (0, -1).into(),
+            Direction::East => (1, 0).into(),
+            Direction::South => (0, 1).into(),
+            Direction::West => (-1, 0).into(),
+        }
+    }
+
+    fn turn_right(&self) -> Self {
+        match self {
+            Direction::North => Self::East,
+            Direction::East => Self::South,
+            Direction::South => Self::West,
+            Direction::West => Self::North,
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Guard {
+    pos: Vec2,
+    dir: Direction,
+}
+
+#[derive(Clone)]
+struct State {
+    guard: Guard,
+    tiles: HashMap<Vec2, Tile>,
+    size: Vec2,
+}
+
+impl State {
+    fn new(input: &str) -> Self {
+        let mut tiles = HashMap::new();
+        let mut guard: Option<Guard> = None;
+
+        let size = (input.lines().next().unwrap().len(), input.lines().count()).into();
+
+        for (y, row) in input.lines().enumerate() {
+            for (x, c) in row.chars().enumerate() {
+                let tile = Tile::new(c);
+                tiles.insert((x, y).into(), tile);
+
+                if c == '^' {
+                    guard = Some(Guard {
+                        pos: (x, y).into(),
+                        dir: Direction::North,
+                    })
+                }
+            }
+        }
+
+        Self {
+            tiles,
+            size,
+            guard: guard.expect("failed to find guard"),
+        }
+    }
+
+    fn take_turn(&mut self) -> bool {
+        if self.guard.pos.x < 0
+            || self.guard.pos.x >= self.size.x
+            || self.guard.pos.y < 0
+            || self.guard.pos.y >= self.size.y
+        {
+            return false;
+        }
+        let next_pos = self.guard.pos + self.guard.dir.get_offset();
+
+        match self.tiles.get(&next_pos) {
+            Some(next) if matches!(next, Tile::Blocked) => {
+                self.guard.dir = self.guard.dir.turn_right()
+            }
+            Some(next) if matches!(next, Tile::Empty) => {
+                self.tiles.insert(next_pos, Tile::Visited(self.guard.dir));
+                self.guard.pos = next_pos;
+            }
+            _ => self.guard.pos = next_pos,
+        }
+
+        return true;
+    }
+
+    fn is_blocked(&self, pos: Vec2) -> bool {
+        self.tiles
+            .get(&pos)
+            .is_some_and(|tile| matches!(tile, Tile::Blocked))
+    }
+
+    fn is_loop(&self, pos: Vec2) -> bool {
+        let mut new_state = self.clone();
+        new_state.tiles.insert(pos, Tile::Blocked);
+
+        loop {
+            if new_state.guard.pos.x < 0
+                || new_state.guard.pos.x >= new_state.size.x
+                || new_state.guard.pos.y < 0
+                || new_state.guard.pos.y >= new_state.size.y
+            {
+                return false;
+            }
+            let next_pos = new_state.guard.pos + new_state.guard.dir.get_offset();
+
+            match new_state.tiles.get(&next_pos) {
+                Some(next) => match next {
+                    Tile::Blocked => new_state.guard.dir = new_state.guard.dir.turn_right(),
+                    Tile::Empty => {
+                        new_state
+                            .tiles
+                            .insert(next_pos, Tile::Visited(new_state.guard.dir));
+                        new_state.guard.pos = next_pos;
+                    }
+                    Tile::Visited(dir) => {
+                        if new_state.guard.dir == *dir {
+                            return true;
+                        } else {
+                            new_state.guard.pos = next_pos;
+                        }
+                    }
+                },
+                _ => new_state.guard.pos = next_pos,
+            }
+        }
+    }
+
+    fn num_visited(&self) -> usize {
+        self.tiles
+            .values()
+            .filter(|tile| matches!(tile, Tile::Visited(_)))
+            .count()
+    }
+}
+
+fn part1(input: &str) -> usize {
+    let mut state = State::new(input);
+    loop {
+        if !state.take_turn() {
+            return state.num_visited();
+        }
+    }
+}
+
+fn part2(input: &str) -> usize {
+    let state = State::new(input);
+    let mut total = 0;
+
+    for x in 0..state.size.x {
+        for y in 0..state.size.y {
+            let pos = Vec2::new(x, y);
+            if pos != state.guard.pos && !state.is_blocked(pos) && state.is_loop(pos) {
+                total += 1;
+            }
+        }
+    }
+
+    return total;
+}
+
+fn main() {
+    let input = utils::read_input_file(6).expect("failed to open input");
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{part1, part2};
+
+    const INPUT: &str = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+
+    #[test]
+    fn day6_part1() {
+        let res = part1(INPUT);
+        assert_eq!(res, 41);
+    }
+
+    #[test]
+    fn day6_part2() {
+        let res = part2(INPUT);
+        assert_eq!(res, 6);
+    }
+}
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,8 +1,52 @@
-use std::{env, fs};
+use std::{env, fmt::Display, fs, ops::Add};
 
 pub fn read_input_file(day: usize) -> std::io::Result<String> {
     let mut current = env::current_dir()?;
     current.push(format!("day{day}"));
     current.push("input.txt");
     fs::read_to_string(current.to_str().unwrap())
+}
+
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub struct Vec2 {
+    pub x: isize,
+    pub y: isize,
+}
+
+impl Display for Vec2 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl Vec2 {
+    pub fn new(x: isize, y: isize) -> Self {
+        Self { x, y }
+    }
+}
+
+impl From<(isize, isize)> for Vec2 {
+    fn from(value: (isize, isize)) -> Self {
+        Self::new(value.0, value.1)
+    }
+}
+
+impl From<(i32, i32)> for Vec2 {
+    fn from(value: (i32, i32)) -> Self {
+        Self::new(value.0 as isize, value.1 as isize)
+    }
+}
+
+impl From<(usize, usize)> for Vec2 {
+    fn from(value: (usize, usize)) -> Self {
+        Self::new(value.0 as isize, value.1 as isize)
+    }
+}
+
+impl Add for Vec2 {
+    type Output = Vec2;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec2::new(self.x + rhs.x, self.y + rhs.y)
+    }
 }
